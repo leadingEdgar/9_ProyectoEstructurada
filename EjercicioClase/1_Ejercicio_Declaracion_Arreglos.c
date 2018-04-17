8ff160a479121404a36e694fbcd8df1a15b26ef8
progest <edgarh2014.94@gmail.com>
2018-04-17 14:41:41 -0500

    Entregando los primeros cuatro ejercicios de programacion

---
+++ EjercicioClase/1_Ejercicio_Declaracion_Arreglos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define TAM 5
+
+int main(void)
+{
+
+	const int array(TAM);
+	int array1[TAM] = {1,2,3,4,5};
+	int array2[TAM] = {1,2,[0]=10,3,4,[1]=0};
+	int array3[] = {1,2,3,4,6};
+
+	/*Areglo de caracteres*/
+	char char1[] = { 'a', 'b', 'c'};
+	char char2[]= {62,63,64,65,66};	
+	char char3[] = {0x63, 0x64, 0x65, 0x66};
+
+	printf("%c\n", char3[0]);
+	
+	return 0;
+}
